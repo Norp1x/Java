bee051f122833df584d97c7caf500023e4c27dec
Norp1x <grzegorz_dudek91@o2.pl>
2023-01-31 10:55:09 +0100

    Simplifying

--- src/Testing.java
+++ src/Testing.java
@@ -1,24 +1,11 @@
 public class Testing {
     public static void main(String[] args) {
-        int [] numbers;
-        numbers = new int[10];
-        numbers[0] = 10;
-        numbers[1] = 20;
-        numbers[2] = 30;
-        numbers[3] = 40;
-        numbers[4] = 50;
-        numbers[5] = 60;
-        numbers[6] = 70;
-        numbers[7] = 80;
-        numbers[8] = 90;
-        numbers[9] = 100;
-
-            int i = 0;
+        int[] numbers = new int[]{10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        for (int tableNumbers : numbers) System.out.println(tableNumbers);
+           /* int i = 0;
             while (i < numbers.length) {
                 System.out.println(numbers[i]);
                 i++;
-            }
-       // for (int tableNumbers : numbers) System.out.println(tableNumbers);
-
+            }*/
     }
 }
